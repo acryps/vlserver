3b4c82b72506fcbfff0f1d4b9320bd4de14121ec
edupes <yo.ey.abc@gmail.com>
2020-10-11 09:45:04 +0200

    fix tools

--- src/config.ts
+++ src/config.ts
@@ -15,39 +15,9 @@ const userConfig = JSON.parse(fs.readFileSync(`${rootFolder}/vlconfig.json`).toS
 
 export const config = {
 	root: rootFolder,
-	context: {
-		outFile: (userConfig.context && userConfig.context.outFile) || "db-context.ts",
+	service: {
+		outFile: (userConfig.context && userConfig.context.outFile) || "services.ts",
 		connection: (userConfig.context && userConfig.context.connection) || {},
 		active: (userConfig.context && userConfig.context.active)
-	},
-	compile: {
-		scan: (userConfig.compile && userConfig.compile.scan) || [
-			"${tsout}",
-		],
-		methods: (userConfig.compile && userConfig.compile.methods) || [
-			"where",
-			"first",
-			"count"
-		],
-		verbose: userConfig.compile ? ("verbose" in userConfig.compile ? userConfig.compile.verbose : false) : false
 	}
 };
-
-// resolve ${tsout} in scanner settings
-for (let scanner = 0; scanner < config.compile.scan.length; scanner++) {
-	if (config.compile.scan[scanner].includes("${tsout}")) {
-		let tsConfigRootFolder = process.cwd();
-
-		while (path.parse(tsConfigRootFolder).root != tsConfigRootFolder && !fs.existsSync(`${tsConfigRootFolder}/tsconfig.json`)) {
-			tsConfigRootFolder = path.resolve(tsConfigRootFolder, "..");
-		}
-
-		if (path.parse(tsConfigRootFolder).root == tsConfigRootFolder) {
-			throw new Error(`No tsconfig.json configuration found in '${process.cwd()}' required to resolve '${config.compile.scan[scanner]}' in vlconfig.json!`);
-		}
-
-		const tsconfig = JSON.parse(fs.readFileSync(`${tsConfigRootFolder}/tsconfig.json`).toString());
-
-		config.compile.scan[scanner] = config.compile.scan[scanner].replace("${tsout}", `${tsConfigRootFolder}/${tsconfig.compilerOptions.outDir}`);
-	}
-}
--- src/tools/index.ts
+++ src/tools/index.ts
@@ -1,34 +1,26 @@
 import fs = require("fs");
-
-import { compileQueries } from "./parser";
-import { createContext } from "./context-generator";
+import { compileServices } from "./compile";
 
 const args = process.argv.slice(2);
 
 switch (args[0]) {
-	case "create-context":
-		createContext();
-
-		break;
-
 	case "compile":
-		compileQueries();
+		compileServices();
 
 		break;
 
 	case "version":
 		const config = JSON.parse(fs.readFileSync(`${__dirname}/../../package.json`).toString());
 
-		console.log(`vlquery v${config.verison}`);
+		console.log(`vlserver v${config.verison}`);
 
 		break;
 
 	default: {
 		console.warn(`invalid command: ${args[0]}`);
 		console.group();
-		console.log("create-context: Create database context");
-		console.log("compile: Compile queries in typescript dist");
-		console.log("version: Print vlquery version");
+		console.log("compile: Compile services");
+		console.log("version: Print vlserver version");
 		console.groupEnd();
 
 		process.exit(1);
