f516b41a21f457051cc50fb4c76d2ef2bac17bf3
edupes <yo.ey.abc@gmail.com>
2020-11-01 17:57:25 +0100

    fix remove unneeded runContext in Manager

--- src/manager.ts
+++ src/manager.ts
@@ -1,7 +1,1 @@
-import { RunContext } from "vlquery";
-
-export class Manager {
-	// holds the context
-	// will be passed on to injected instances
-	runContext: RunContext;
-}
+export class Manager {}
