790a6c71de4e79379edd252563c280d25a456a56
Levi Hechenberger <mac@Mac-Pro-Levi.local>
2020-12-07 14:06:22 +0100

    add angular adapter type convert

--- src/tools/adapters/angular.ts
+++ src/tools/adapters/angular.ts
@@ -16,6 +16,39 @@ export class ${viewModel.name} {
 
 		return `${name}: ${property.propertyType}${(property.fetch && property.fetch.many) ? "[]" : ""};`;
 	}).join("\n\t")}
+
+	private static $build(raw) {
+		const item = new ${viewModel.name}();
+		${Object.keys(viewModel.properties).map(name => {
+			const property = viewModel.properties[name];
+
+			if (viewModel.properties[name].fetch) {
+				if (viewModel.properties[name].fetch.single) {
+					return `item.${name} = raw.${name} ? null : ${viewModel.properties[name].fetch.single}["$build"](raw.${name})`;
+				} else {
+					return `item.${name} = raw.${name} ? null : raw.${name}.map(i => ${viewModel.properties[name].fetch.single}["$build"](i))`;
+				}
+			} else {
+				if (viewModel.properties[name].propertyType == "boolean") {
+					return `item.${name} = !!raw.${name}`;
+				}
+
+				if (viewModel.properties[name].propertyType == "string") {
+					return `item.${name} = raw.${name} === null ? null : \`\${raw.${name}}\``;
+				}
+
+				if (viewModel.properties[name].propertyType == "number") {
+					return `item.${name} = raw.${name} === null ? null : +raw.${name}`;
+				}
+
+				if (viewModel.properties[name].propertyType == "Date") {
+					return `item.${name} = raw.${name} === null ? null : new Date(raw.${name})`;
+				}
+			}
+		}).join("\n\t\t")}
+
+		return item;
+	}
 }
 `.trim()).join("\n\n")}
 
@@ -35,6 +68,26 @@ export class ${controller.name} {
 			${(route.returnType.length || route.returnType[0] == "void") ? `
 			
 			if ("data" in r) {
+				const d = r.data;
+
+				return ${route.returnType.slice(0, route.returnType.length - 1).map(t => `d.map(d => `)}${(() => {
+					const type = route.returnType[route.returnType.length - 1];
+
+					if (type == "boolean") {
+						return "!!d";
+					} else if (type == "string") {
+						return "d === null ? null : `${d}`";
+					} else if (type == "number") {
+						return "d === null ? null : +d";
+					} else if (type == "Date") {
+						return "d === null ? null : new Date(d)";
+					} else {
+						return `${type}["$build"](d)`;
+					} 
+				})()}${")".repeat(route.returnType.length - 1)}
+
+				r.data.map(i => i.map())
+
 				return r.data as ${route.returnType.slice(0, route.returnType.length - 1).map(t => `${t}<`)}${route.returnType[route.returnType.length - 1]}${">".repeat(route.returnType.length - 1)};
 			} else if ("error" in r) {
 				throw new Error(r.error);
