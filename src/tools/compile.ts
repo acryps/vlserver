5c81f4858afeea63490d8d40af373b7d3b3a5825
Levi Hechenberger <mac@Mac-Pro-Levi.local>
2020-12-07 13:59:07 +0100

    fix type convert

--- src/tools/compile.ts
+++ src/tools/compile.ts
@@ -443,15 +443,15 @@ ViewModel.mappings = {
 					}
 
 					if (viewModel.properties[name].propertyType == "string") {
-						return `model.${name} = viewModel.${name} === null ? null : \`\${data.${name}}\``;
+						return `model.${name} = viewModel.${name} === null ? null : \`\${viewModel.${name}}\``;
 					}
 
 					if (viewModel.properties[name].propertyType == "number") {
-						return `model.${name} = viewModel.${name} === null ? null : +data.${name}`;
+						return `model.${name} = viewModel.${name} === null ? null : +viewModel.${name}`;
 					}
 
 					if (viewModel.properties[name].propertyType == "Date") {
-						return `model.${name} = viewModel.${name} === null ? null : new Date(data.${name})`;
+						return `model.${name} = viewModel.${name} === null ? null : new Date(viewModel.${name})`;
 					}
 				}
 			})()});`).join("\n\t\t\t")}
