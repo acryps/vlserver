aaa83935010eeea4ce6d65d23864996245f348c8
Levi Hechenberger <mac@Mac-Pro-Levi.local>
2020-12-11 12:43:47 +0100

    add $build to native adapter

--- src/tools/adapters/native.ts
+++ src/tools/adapters/native.ts
@@ -6,7 +6,7 @@ export class NativeServiceAdapter extends ServiceAdapter {
 		const controllers = routes.map(r => r.controller).filter((c, i, a) => a.indexOf(c) == i);
 
 		fs.writeFileSync(this.outFile, `
-		
+
 ${viewModels.map(viewModel => `
 export class ${viewModel.name} {
 	${Object.keys(viewModel.properties).map(name => {
@@ -14,8 +14,42 @@ export class ${viewModel.name} {
 
 		return `${name}: ${property.propertyType}${(property.fetch && property.fetch.many) ? "[]" : ""};`;
 	}).join("\n\t")}
+
+	private static $build(raw) {
+		const item = new ${viewModel.name}();
+		${Object.keys(viewModel.properties).map(name => {
+			const property = viewModel.properties[name];
+
+			if (viewModel.properties[name].fetch) {
+				if (viewModel.properties[name].fetch.single) {
+					return `item.${name} = raw.${name} ? ${viewModel.properties[name].fetch.single}["$build"](raw.${name}) : null`;
+				} else {
+					return `item.${name} = raw.${name} ? raw.${name}.map(i => ${viewModel.properties[name].fetch.many}["$build"](i)) : null`;
+				}
+			} else {
+				if (viewModel.properties[name].propertyType == "boolean") {
+					return `item.${name} = !!raw.${name}`;
+				}
+
+				if (viewModel.properties[name].propertyType == "string") {
+					return `item.${name} = raw.${name} === null ? null : \`\${raw.${name}}\``;
+				}
+
+				if (viewModel.properties[name].propertyType == "number") {
+					return `item.${name} = raw.${name} === null ? null : +raw.${name}`;
+				}
+
+				if (viewModel.properties[name].propertyType == "Date") {
+					return `item.${name} = raw.${name} ? new Date(raw.${name}) : null`;
+				}
+			}
+		}).join("\n\t\t")}
+		
+		return item;
+	}
 }
 `.trim()).join("\n\n")}
+
 
 ${controllers.map(controller => `
 export class ${controller.name} {
@@ -29,28 +63,42 @@ export class ${controller.name} {
 			method: "post",
 			body: data
 		}).then(res => res.json()).then(r => {
-			${(route.returnType.length || route.returnType[0] == "void") ? `
-			
-			if ("data" in r) {
-				return r.data as ${route.returnType.slice(0, route.returnType.length - 1).map(t => `${t}<`)}${route.returnType[route.returnType.length - 1]}${">".repeat(route.returnType.length - 1)};
-			} else if ("error" in r) {
-				throw new Error(r.error);
-			}
-			
-			`.trim() : `
+			${((!route.returnType.length || route.returnType[0] == "void") ? `
 			
 			if ("error" in r) {
 				throw new Error(r.error);
 			}
 			
-			`.trim()}
+			` : `
+
+			if ("data" in r) {
+				const d = r.data;
+
+				return ${route.returnType.slice(0, route.returnType.length - 1).map(t => `d.map(d => `)}${(() => {
+					const type = route.returnType[route.returnType.length - 1];
+
+					if (type == "boolean") {
+						return "!!d";
+					} else if (type == "string") {
+						return "d === null ? null : `${d}`";
+					} else if (type == "number") {
+						return "d === null ? null : +d";
+					} else if (type == "Date") {
+						return "d === null ? null : new Date(d)";
+					} else {
+						return `d === null ? null : ${type}["$build"](d)`;
+					} 
+				})()}${")".repeat(route.returnType.length - 1)};
+			} else if ("error" in r) {
+				throw new Error(r.error);
+			}
+			
+			`).trim()}
 		});
 	}
 	
 	`.trim()).join("\n\n\t")}
 }
-`.trim()).join("\n\n")}
-		
-		`.trim());
+`.trim()).join("\n\n")}`.trim());
 	}
 }
