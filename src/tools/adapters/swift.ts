8bc16e39e79911ae86514dbfafa94625599ed30f
edupes <yo.ey.abc@gmail.com>
2021-05-16 11:52:54 +0200

    fix swift format

--- src/tools/adapters/swift.ts
+++ src/tools/adapters/swift.ts
@@ -13,6 +13,7 @@ export class SwiftServiceAdapter extends ServiceAdapter {
 		const controllers = routes.map(r => r.controller).filter((c, i, a) => a.indexOf(c) == i);
 
 		fs.writeFileSync(this.outFile, `
+import Foundation
 
 class Service {
 	var baseUrl = ""
@@ -32,7 +33,7 @@ class RequestBody {
 		self.body = NSMutableData()
 	}
 
-	var header() {
+	var header: String {
 		return "multipart/form-data; boundary=----\\(body.boundary)"
 	}
 	
@@ -61,13 +62,18 @@ ${controllers.map(controller => `
 class ${controller.name} : Service {
 	${routes.filter(r => r.controller == controller).map(route => `
 
-	func ${route.name}(${route.parameters.map(parameter => `${parameter.name}: ${parameter.isArray ? "[" : ""}${this.typeMappings[parameter.type] || parameter.type}${parameter.isArray ? "]" : ""}`)}, completionHandler: @escaping (Error?, ${
-        route.returnType.slice(0, route.returnType.length - 1).map(t => `[`)
-    }${
-        route.returnType[route.returnType.length - 1]
-    }${
-        "]".repeat(route.returnType.length - 1)
-    }?) -> Void) {
+	func ${route.name}(${[
+		...route.parameters.map(
+			parameter => `${parameter.name}: ${parameter.isArray ? "[" : ""}${this.typeMappings[parameter.type] || parameter.type}${parameter.isArray ? "]" : ""}`
+		),
+		`completionHandler: @escaping (Error?, ${
+			route.returnType.slice(0, route.returnType.length - 1).map(t => `[`)
+		}${
+			route.returnType[route.returnType.length - 1]
+		}${
+			"]".repeat(route.returnType.length - 1)
+		}?) -> Void`
+	].join(", ")}) {
 		let endpoint = URL(string: toURL(route: ${JSON.stringify(route.id)}))
 		var request = URLRequest(url: endpoint!)
 		request.httpMethod = "POST"
@@ -93,23 +99,21 @@ class ${controller.name} : Service {
 				if res["data"] != nil {
 					let result = res["data"]
 
-					return ${route.returnType.slice(0, route.returnType.length - 1).map(t => `result.map({ result in { return `)}${(() => {
+					completionHandler(nil, ${route.returnType.slice(0, route.returnType.length - 1).map(t => `(result as! [Any]).map({ result in return `)}${(() => {
 						const type = route.returnType[route.returnType.length - 1];
 	
 						if (type == "boolean") {
 							return "!!result";
 						} else if (type == "string") {
-							return "result == nil ? nil : result as! String`";
+							return "result == nil ? nil : result as! String"
 						} else if (type == "number") {
-							return "result == nil ? nil : result as! Double";
+							return "result == nil ? nil : result as! Double"
 						} else if (type == "Date") {
-							return "result == nil ? nil : result as! ISO8601DateFormatter().date(from: result)";
+							return "result == nil ? nil : result as! ISO8601DateFormatter().date(from: result)"
 						} else {
-							return `result == nil ? nil : JSONDecoder().decode(${type}, from: result)`;
+							return `result == nil ? nil : JSONDecoder().decode(${type}, from: result as! Data)`
 						} 
-					})()}${"})".repeat(route.returnType.length - 1)};
-					
-					completionHandler(nil, Double(result as! Double))
+					})()}${"})".repeat(route.returnType.length - 1)})
 				} else if res["aborted"] != nil {
 					throw ServiceError(message: "request aborted by server")
 				} else if res["error"] != nil {
@@ -126,6 +130,6 @@ class ${controller.name} : Service {
 
 `.trim()).join("\n\n\t")}
 	}
-`.trim()).join("\n\n")}`);
+`.trim()).join("\n\n")}`.trim());
 	}
 }
