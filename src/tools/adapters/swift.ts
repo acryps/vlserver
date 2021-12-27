6987233980588ba0e71a48bd3dff0ead10debef1
Levi Hechenberger Di Valentino <levi.hechenberger@gmail.com>
2021-12-27 14:27:11 +0100

    add swift alternative init

--- src/tools/adapters/swift.ts
+++ src/tools/adapters/swift.ts
@@ -77,6 +77,20 @@ class ${viewModel.name} : Codable {
 
 		return `var ${name}: ${isArray ? "[" : ""}${this.getType(property.propertyType)}${isArray ? "]" : ""};`;
 	}).join("\n\t")}
+
+	public init(${Object.keys(viewModel.properties).map(name => {
+		const property = viewModel.properties[name];
+		const isArray = property.fetch && property.fetch.many;
+
+		return `${name}: ${isArray ? "[" : ""}${this.getType(property.propertyType)}${isArray ? "]" : ""}`;
+	}).join(", ")}) {
+        ${Object.keys(viewModel.properties).map(name => {
+			const property = viewModel.properties[name];
+			const isArray = property.fetch && property.fetch.many;
+	
+			return `self.${name} = ${name}`;
+		}).join("\n\t\t")}
+    }
 }
 `.trim()).join("\n\n")}
 
